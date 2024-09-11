4b9770893b47bafd8432659707686db6e552fe4b
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-11 21:02:19 +0630

    add api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="max-w-2xl mx-auto">
+      <body>
+        <div className="max-w-2xl mx-auto p-4">
           <Nav />
           {children}
         </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between px-6 py-2 border-b border-b-gray-300">
+    <nav className="flex justify-between px-6 py-2 border-b border-b-gray-300 shadow">
       <div>Next-Auth</div>
       <div>
         <button>Sign in</button>

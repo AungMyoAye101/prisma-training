9b38587070adbcac05f89c9a069a5c0aaa4a91eb
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-15 20:33:52 +0630

    add post grid

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,8 +2,57 @@ import React from "react";
 
 const Post = () => {
   return (
-    <section>
-      <div>
+    <section className=" w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="flex-col p-4 rounded shadow-md">
+        <div className="flex">
+          <img src="f" alt="user profile" />
+          <div>Justin </div>
+        </div>
+        <div>I love you more than i</div>
+      </div>
+      <div className="flex-col p-4 rounded shadow-md">
+        <div className="flex">
+          <img src="f" alt="user profile" />
+          <div>Justin </div>
+        </div>
+        <div>I love you more than i</div>
+      </div>
+      <div className="flex-col p-4 rounded shadow-md">
+        <div className="flex">
+          <img src="f" alt="user profile" />
+          <div>Justin </div>
+        </div>
+        <div>I love you more than i</div>
+      </div>
+      <div className="flex-col p-4 rounded shadow-md">
+        <div className="flex">
+          <img src="f" alt="user profile" />
+          <div>Justin </div>
+        </div>
+        <div>I love you more than i</div>
+      </div>
+      <div className="flex-col p-4 rounded shadow-md">
+        <div className="flex">
+          <img src="f" alt="user profile" />
+          <div>Justin </div>
+        </div>
+        <div>I love you more than i</div>
+      </div>
+      <div className="flex-col p-4 rounded shadow-md">
+        <div className="flex">
+          <img src="f" alt="user profile" />
+          <div>Justin </div>
+        </div>
+        <div>I love you more than i</div>
+      </div>
+      <div className="flex-col p-4 rounded shadow-md">
+        <div className="flex">
+          <img src="f" alt="user profile" />
+          <div>Justin </div>
+        </div>
+        <div>I love you more than i</div>
+      </div>
+      <div className="flex-col p-4 rounded shadow-md">
         <div className="flex">
           <img src="f" alt="user profile" />
           <div>Justin </div>

5a34fc596c0ebdb7cec58f0344e3da109ae787bc
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-15 20:31:26 +0630

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+import Post from "@/components/Post";
+
 export default async function Home() {
   return (
-    <section className="flex justify-center items-center min-h-screen">
+    <section className="flex flex-col justify-center items-center min-h-screen">
       <div>
-        <h1>user</h1>
-        <h2>email</h2>
+        <h1 className="text-xl ">There is will </h1>
+        <p className="text-4xl font-serif">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          excepturi, recusandae eos impedit harum fugiat nobis. Numquam,
+          mollitia nobis tenetur id, porro deleniti voluptas dolores modi quos
+          placeat, animi dignissimos.
+        </p>
       </div>
+      <Post />
     </section>
   );
 }

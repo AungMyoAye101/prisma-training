df85123e9ac30eebda787232723bf4891b2a4f2f
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-15 20:37:12 +0630

    update post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,56 +3,49 @@ import React from "react";
 const Post = () => {
   return (
     <section className=" w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <div className="flex-col p-4 rounded shadow-md">
+      <div className="flex-col p-4 rounded shadow-md border border-gray-300">
         <div className="flex">
           <img src="f" alt="user profile" />
           <div>Justin </div>
         </div>
         <div>I love you more than i</div>
       </div>
-      <div className="flex-col p-4 rounded shadow-md">
+      <div className="flex-col p-4 rounded shadow-md border border-gray-300">
         <div className="flex">
           <img src="f" alt="user profile" />
           <div>Justin </div>
         </div>
         <div>I love you more than i</div>
       </div>
-      <div className="flex-col p-4 rounded shadow-md">
+      <div className="flex-col p-4 rounded shadow-md border border-gray-300">
         <div className="flex">
           <img src="f" alt="user profile" />
           <div>Justin </div>
         </div>
         <div>I love you more than i</div>
       </div>
-      <div className="flex-col p-4 rounded shadow-md">
+      <div className="flex-col p-4 rounded shadow-md border border-gray-300">
         <div className="flex">
           <img src="f" alt="user profile" />
           <div>Justin </div>
         </div>
         <div>I love you more than i</div>
       </div>
-      <div className="flex-col p-4 rounded shadow-md">
+      <div className="flex-col p-4 rounded shadow-md border border-gray-300">
         <div className="flex">
           <img src="f" alt="user profile" />
           <div>Justin </div>
         </div>
         <div>I love you more than i</div>
       </div>
-      <div className="flex-col p-4 rounded shadow-md">
+      <div className="flex-col p-4 rounded shadow-md border border-gray-300">
         <div className="flex">
           <img src="f" alt="user profile" />
           <div>Justin </div>
         </div>
         <div>I love you more than i</div>
       </div>
-      <div className="flex-col p-4 rounded shadow-md">
-        <div className="flex">
-          <img src="f" alt="user profile" />
-          <div>Justin </div>
-        </div>
-        <div>I love you more than i</div>
-      </div>
-      <div className="flex-col p-4 rounded shadow-md">
+      <div className="flex-col p-4 rounded shadow-md border border-gray-300">
         <div className="flex">
           <img src="f" alt="user profile" />
           <div>Justin </div>

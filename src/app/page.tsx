eb5ba15240dd6e79c64157165c5e3a53080a2be1
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-15 20:35:05 +0630

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,19 @@ import Post from "@/components/Post";
 
 export default async function Home() {
   return (
-    <section className="flex flex-col justify-center items-center min-h-screen">
-      <div>
-        <h1 className="text-xl ">There is will </h1>
-        <p className="text-4xl font-serif">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          excepturi, recusandae eos impedit harum fugiat nobis. Numquam,
-          mollitia nobis tenetur id, porro deleniti voluptas dolores modi quos
-          placeat, animi dignissimos.
-        </p>
-      </div>
+    <>
+      <section className="flex flex-col justify-center items-center min-h-screen">
+        <div>
+          <h1 className="text-xl ">There is will </h1>
+          <p className="text-4xl font-serif">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            excepturi, recusandae eos impedit harum fugiat nobis. Numquam,
+            mollitia nobis tenetur id, porro deleniti voluptas dolores modi quos
+            placeat, animi dignissimos.
+          </p>
+        </div>
+      </section>
       <Post />
-    </section>
+    </>
   );
 }

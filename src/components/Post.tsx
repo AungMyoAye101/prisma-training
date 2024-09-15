3f41236727b4b518c3399d1d6d5874d04798e15e
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-15 20:28:22 +0630

    add post components

---
+++ src/components/Post.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <section>
+      <div>
+        <div className="flex">
+          <img src="f" alt="user profile" />
+          <div>Justin </div>
+        </div>
+        <div>I love you more than i</div>
+      </div>
+    </section>
+  );
+};
+
+export default Post;
